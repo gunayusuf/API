aed4a093751204425f7cd3902221c0b1f420577b
Yusuf Gunay <gunayusuf099@gmail.com>
2022-07-07 23:23:51 +0300

    07.07.22

--- src/test/java/get_requests/Get06.java
+++ src/test/java/get_requests/Get06.java
@@ -1,9 +1,20 @@
 package get_requests;
 
-public class Get06 {
+import base_urls.HerokuAppBaseUrl;
+import io.restassured.http.ContentType;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import org.junit.Test;
+import org.testng.asserts.SoftAssert;
+
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
+import static org.junit.Assert.assertEquals;
+
+public class Get06 extends HerokuAppBaseUrl {
     /*
-        Given
-            https://restful-booker.herokuapp.com/booking/5
+       Given
+            https://restful-booker.herokuapp.com/booking/101
         When
             User send a GET request to the URL
         Then
@@ -12,13 +23,84 @@ public class Get06 {
             Response content type is “application/json”
         And
             Response body should be like;
-            {
-                "firstname": "Mary",
-                "lastname": "Jackson",
-                "totalprice": 111,
-                "depositpaid": false,
-                "bookingdates": { "checkin": "2017-05-23",
-                                  "checkout":"2019-07-02" }
-            }
+          {
+        "firstname": "GGS",
+        "lastname": "FINCH",
+        "totalprice": 111,
+        "depositpaid": true,
+        "bookingdates": {
+        "checkin": "2018-01-01",
+        "checkout": "2019-01-01"
+          }
+
      */
+
+    @Test
+    public void get01() {
+
+    //1 Step: Set the Url
+        spec.pathParams("first","booking", "second",101);
+
+    //2 Step: Set the expected data
+
+    //3 Step: Send the request and get response
+   Response response = given().spec(spec).when().get("/{first}/{second}");
+  // response.prettyPrint();
+
+    //4 Step: Do Assertion
+
+        //1.YOL
+        response.
+                then()
+                .assertThat()
+                .statusCode(200)
+                .contentType(ContentType.JSON)
+                .body("firstname",equalTo("GGS"),
+                        "lastname",equalTo("FINCH"),
+                        "totalprice",equalTo(111),
+                        "depositpaid",equalTo(true),
+                        "bookingdates.checkin",equalTo("2018-01-01"),
+                        "bookingdates.checkout",equalTo("2019-01-01"));
+
+
+                //NOT NESTEDLARDA . (NOKTA) koyarak ic ice olan verilere ulasabiliriz
+
+        //2.YOL ==> JsonPath Class kullanacagiz
+
+        JsonPath jsonPath = response.jsonPath();  //json = new JsonPath(response.asInputStream()); bu sekilde de olusturulabilir
+        assertEquals("GGS",jsonPath.getString("firstname"));
+        assertEquals("FINCH",jsonPath.getString("lastname"));
+        assertEquals(111,jsonPath.getInt("totalprice"));
+        assertEquals(true,jsonPath.getBoolean("depositpaid"));
+        assertEquals("2018-01-01",jsonPath.getString("bookingdates.checkin"));
+        assertEquals("2019-01-01",jsonPath.getString("bookingdates.checkout"));
+        //Bu sekilde hard Assertion oldu
+
+
+        //3.YOL: Soft Assertion
+        //Soft Assertion icin 3 adim izlenir;
+
+        //1) SoftAssert Objesi olusturulur
+        SoftAssert softAssert=new SoftAssert();
+
+        //2) Olusturdugumuz Obje araciligi ile Assertion yapacagiz
+        softAssert.assertEquals(jsonPath.getString("firstname"),"GGS","firstname eslesmedi");
+
+        softAssert.assertEquals(jsonPath.getString("lastname"),"FINCH");
+        softAssert.assertEquals(jsonPath.getString("totalprice"),111);
+
+        //3) assertAll() methodu kullanilir. Aksi takdirde assert islemi gerceklesmez.
+        softAssert.assertAll();
+
+
+
+
+
+
+
+
+
+
+
+    }
 }
